afd0b6b9a5af0476d3e1f7137571a06098b2021c
dogboy <1732164513@qq.com>
2017-03-11 10:45:40 +0800

    开始后端

--- redister.php
+++ redister.php
@@ -1,2 +1,21 @@
-<?php echo "hello";
+<?php echo "<body background='./image/index.png'>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<center><h1 style='color:blue'>hello,功能暂未开放</h1></center>
+</body>";
 ?>
+<script type="text/javascript">function redrect() 
+{ 
+window.location = "index.html"; 
+} 
+window.setTimeout(redrect,3000);
+</script>
